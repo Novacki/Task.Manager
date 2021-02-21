55cb3bc39cc34454b499cd18286b4c74578a78c7
FERNANDO NOVACKI FREITAS <fernando.freitas3@fatec.sp.gov.br>
2021-02-21 19:15:57 -0300

    Added devise and model user

--- task-manager-api/config/routes.rb
+++ task-manager-api/config/routes.rb
@@ -1,3 +1,12 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/'  do -> Como não configurei o host, não irei usar com restrições.
+  namespace :api, defaults: {format: :json}, path: '/'  do
+
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do 
+    end
+  end
 end
