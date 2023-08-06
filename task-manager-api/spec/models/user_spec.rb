939ee6dac057499e48ce748d044343b8c0913787
Novacki <novackifernando@hotmail.com>
2023-08-06 16:30:23 -0300

    wip

--- task-manager-api/spec/models/user_spec.rb
+++ task-manager-api/spec/models/user_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user) }
+  # subject { build(:user) }
+  # before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
-  
+  it { expect(subject).to respond_to(:email) }
+  # it { expect(subject).to respond_to(:name) }
+  it { expect(subject).to respond_to(:password) }
+  it { expect(subject).to respond_to(:password_confirmation) }
+  # it { expect(subject).to be_valid }
+
+  # it { expect(subject).to respond_to(:email) }
+  it { is_expected.to respond_to(:email) }
 end
